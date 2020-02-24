c3abd70c708a097e73c864a5f7e98932b96f5f7c
Yoo Hwa Park <16ypark@kaist.ac.kr>
2020-02-24 16:19:00 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Yoo Hwa Park on 2020/02/24.
+//  Copyright © 2020 Yoo Hwa Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
